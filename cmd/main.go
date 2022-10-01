828dffaf363122273558c9c2c48f901fba1ed627
bilginyuksel <bilgin.yuksel96@gmail.com>
2022-10-01 11:42:00 +0300

    Update config and app startup setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,16 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"gotemplate/internal/subscription"
+	subscription_adapter "gotemplate/internal/subscription/adapter"
+	subscription_port "gotemplate/internal/subscription/port"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
 )
 
@@ -10,6 +20,25 @@ func main() {
 		panic(err)
 	}
 
-	conf := readConfig()
+	env := os.Getenv("APP_ENV")
+
+	conf := readConfig(env)
 	logger.Info("application started..", zap.Any("conf", conf))
+
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(conf.Mongo.URI))
+	if err != nil {
+		panic(err)
+	}
+
+	subscriptionRepository := subscription_adapter.NewMongo(client.Database("gotemplate").Collection("subscriptions"))
+	subscriptionService := subscription.NewService(subscriptionRepository, nil)
+	subscriptionRestHandler := subscription_port.NewSubscriptionRestHandler(subscriptionService)
+
+	e := echo.New()
+
+	e.POST("/subscriptions", subscriptionRestHandler.CreateSubscription)
+
+	if err := e.Start(fmt.Sprintf(":%d", conf.Port)); err != nil {
+		logger.Fatal("shutting down the server", zap.Error(err))
+	}
 }
